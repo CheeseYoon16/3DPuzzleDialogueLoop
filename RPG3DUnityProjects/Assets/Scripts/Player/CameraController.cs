435271a31c2a1ad4c4e650bb7713f2372a0f62fb
GalsCressendo <galuhkmj@gmail.com>
2025-06-19 13:52:20 +0700

    modify camera movement

--- RPG3DUnityProjects/Assets/Scripts/Player/CameraController.cs
+++ RPG3DUnityProjects/Assets/Scripts/Player/CameraController.cs
@@ -6,15 +6,17 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public Vector2 turn;
+    public Vector3 moveDirection;
+    public float turnSensitivity = 1.0f;
     public float zoomSpeed = 4f;
     public float minZoom = 5f;
     public float maxZoom = 15f;
 
-    public float yawSpeed = 100f;
+    public float moveSpeed = 100f;
 
     public float pitch = 2f;
     float currentZoom = 10f;
-    float currentYaw = 0;
 
     private bool disabledMovement = false;
 
@@ -25,17 +27,29 @@ public class CameraController : MonoBehaviour
         currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
 
-        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+        if(Input.GetKey(KeyCode.Mouse2))
+        {
+            Debug.Log("Middle mouse button pressed");
+            turn.x += Input.GetAxis("Mouse X") * turnSensitivity;
+            turn.y += Input.GetAxis("Mouse Y") * turnSensitivity;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right
+        float vertical = Input.GetAxis("Vertical");     // W/S or Up/Down
+
+        moveDirection = (transform.forward * vertical + transform.right * horizontal);
+        moveDirection.y = 0; // keep movement horizontal
+
 
     }
     private void LateUpdate()
     {
         if (disabledMovement) return;
 
-        transform.position = target.position - offset * currentZoom;
-        transform.LookAt(target.position + Vector3.up * pitch);
+        Quaternion rotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        Vector3 desiredPosition = target.position - (rotation * offset * currentZoom);
 
-        transform.RotateAround(target.position, Vector3.up, currentYaw);
+        transform.position = desiredPosition;
     }
 
     public void SetDisableMovement(bool isEnabled)
